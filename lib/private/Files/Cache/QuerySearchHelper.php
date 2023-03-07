bd7bf4214e68bd21dc88d13f3686d2bb36bb820f
Dariusz Olszewski <starypatyk@users.noreply.github.com>
2023-03-07 23:15:23 +0100

    Use SQL IN operator for single file shares
    Signed-off-by: Dariusz Olszewski <starypatyk@users.noreply.github.com>
    (cherry picked from commit 0485c072f5645b616521f32ab3501c791f3f16d1)

--- lib/private/Files/Cache/QuerySearchHelper.php
+++ lib/private/Files/Cache/QuerySearchHelper.php
@@ -29,6 +29,7 @@ use OC\Files\Cache\Wrapper\CacheJail;
 use OC\Files\Node\Root;
 use OC\Files\Search\QueryOptimizer\QueryOptimizer;
 use OC\Files\Search\SearchBinaryOperator;
+use OC\Files\Search\SearchComparison;
 use OC\SystemConfig;
 use OCP\DB\QueryBuilder\IQueryBuilder;
 use OCP\Files\Cache\ICache;
@@ -37,6 +38,7 @@ use OCP\Files\Folder;
 use OCP\Files\IMimeTypeLoader;
 use OCP\Files\Mount\IMountPoint;
 use OCP\Files\Search\ISearchBinaryOperator;
+use OCP\Files\Search\ISearchComparison;
 use OCP\Files\Search\ISearchQuery;
 use OCP\IDBConnection;
 use Psr\Log\LoggerInterface;
@@ -78,10 +80,40 @@ class QuerySearchHelper {
 		);
 	}
 
-	protected function applySearchConstraints(CacheQueryBuilder $query, ISearchQuery $searchQuery, array $caches): void {
-		$storageFilters = array_values(array_map(function (ICache $cache) {
+	private function generateStorageFilters(array $caches): array {
+		// Pick up single file shares to prepare more efficient query
+		$storageToPathsMap = [];
+		$otherCaches = [];
+		foreach ($caches as $cache) {
+			if (($cache instanceof \OCA\Files_Sharing\Cache) and $cache->isFileShare()) {
+				$storage = $cache->getNumericStorageId();
+				$storageToPathsMap[$storage][] = $cache->getGetUnjailedRoot();
+			} else {
+				$otherCaches[] = $cache;
+			}
+		}
+
+		// Create filters for single file shares
+		$singleFileFilters = [];
+		foreach ($storageToPathsMap as $storage => $paths) {
+			$singleFileFilters[] = new SearchBinaryOperator(
+				ISearchBinaryOperator::OPERATOR_AND,
+				[
+					new SearchComparison(ISearchComparison::COMPARE_EQUAL, 'storage', $storage),
+					new SearchComparison(ISearchComparison::COMPARE_IN, 'path_hash', array_map(fn ($path) => md5($path), $paths))
+				]
+			);
+		}
+
+		$storageOtherFilters = array_map(function (ICache $cache) {
 			return $cache->getQueryFilterForStorage();
-		}, $caches));
+		}, $otherCaches);
+
+		return array_merge($storageOtherFilters, $singleFileFilters);
+	}
+
+	protected function applySearchConstraints(CacheQueryBuilder $query, ISearchQuery $searchQuery, array $caches): void {
+		$storageFilters = $this->generateStorageFilters($caches);
 		$storageFilter = new SearchBinaryOperator(ISearchBinaryOperator::OPERATOR_OR, $storageFilters);
 		$filter = new SearchBinaryOperator(ISearchBinaryOperator::OPERATOR_AND, [$searchQuery->getSearchOperation(), $storageFilter]);
 		$this->queryOptimizer->processOperator($filter);
